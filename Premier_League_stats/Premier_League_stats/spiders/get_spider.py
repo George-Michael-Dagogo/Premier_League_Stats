96482c8493eb61bcd19b07618ab0cf0045108163
GeeKaizer <georgemichaeldagogo@gmail.com>
2023-01-02 00:37:10 +0000

    latest

--- Premier_League_stats/Premier_League_stats/spiders/get_spider.py
+++ Premier_League_stats/Premier_League_stats/spiders/get_spider.py
@@ -2,6 +2,15 @@ import scrapy
 
 class GetSpider(scrapy.Spider):
     name = 'player_stats'
-    start_url = [
-        'https://www.premierleague.com/stats/top/players/goals'
+    start_urls = [
+        'https://www.premierleague.com/stats/top/players/goal_assist'
     ]
+    custom_settings = {
+        #'DOWNLOAD_DELAY': 5 # 2 seconds of delay
+        }
+
+    def parse(self, response):
+        player_name = response.css('a.playerName > strong::text').extract()
+        yield {'Player_name': player_name} 
+        club_name = response.css('a.statNameSecondary::text').extract()
+        yield {'Club_name': club_name} 
