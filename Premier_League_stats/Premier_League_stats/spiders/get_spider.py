eba6ee2452f6848cf1c600f8fd5f7dcb7c431b9e
GeeKaizer <georgemichaeldagogo@gmail.com>
2023-01-03 02:20:38 +0000

    works

--- Premier_League_stats/Premier_League_stats/spiders/get_spider.py
+++ Premier_League_stats/Premier_League_stats/spiders/get_spider.py
@@ -1,27 +1,20 @@
 import scrapy
-import json 
+
 
 class GetSpider(scrapy.Spider):
     name = 'player_stats'
-    #allowed_domains = ['https://www.premierleague.com/players']
-    page = 1
-    #api_url = 'https://footballapi.pulselive.com/football/players?pageSize=30&compSeasons=489&altIds=true&page=22&type=player&id=-1&compSeasonId=489'
-    start_urls = ['https://footballapi.pulselive.com/football/players?pageSize=30&compSeasons=489&altIds=true&page=1&type=player&id=-1&compSeasonId=489']
-
+    start_urls = [
+        'https://www.premierleague.com/players'
+    ]
     custom_settings = {
-        #'DOWNLOAD_DELAY': 5
+        #'DOWNLOAD_DELAY': 10 # 2 seconds of delay
         }
 
     def parse(self, response):
-        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
-
-        data = json.loads(response.text)
-        for quote in data["quotes"]:
-            yield {"content": quote["text"]}
-        if data["has_next"]:
-                self.page += 1
-                url = f"https://footballapi.pulselive.com/football/players?pageSize=30&compSeasons=489&altIds=true&page={self.page}&type=player&id=-1&compSeasonId=489"
-                yield scrapy.Request(url=url, callback=self.parse, headers=headers)
-    
-
-    
+        for play in response.css('tbody.dataContainer.indexSection > tr'):
+            yield {
+                    'player': play.css('a.playerName::text').get(),
+                    'position': play.css('tr > td.hide-s::text').get(),
+                    'player_country': play.css('tr > td.hide-s > span.playerCountry::text').getall(),
+                    }
+         
