5076c341eafbec6465a399a90587d31517503d3e
GeeKaizer <georgemichaeldagogo@gmail.com>
2023-01-30 21:12:01 +0000

    jjjj

--- Updating_with_BS4/push_to_database.py
+++ Updating_with_BS4/push_to_database.py
@@ -27,7 +27,10 @@ DROP TABLE IF EXISTS
  all_time_winner_club,
  all_time_scorers_penalty,
  all_time_scorers_own_goal,
- goals_per_season
+ goals_per_season,
+ top_scorers_seasons,
+ record_wins,
+ top_scorers_all_time
  ;
 """
 cursor.execute(drop_tables) 
@@ -139,6 +142,59 @@ CREATE TABLE IF NOT EXISTS goals_per_season (
   matches INT NOT NULL,
   average_goal FLOAT NOT NULL
   );
+
+CREATE TABLE IF NOT EXISTS top_scorers_seasons (
+  player_id UUID ,
+  season VARCHAR(45),
+  team_id UUID,
+  seasons VARCHAR(45) NOT NULL,
+  player VARCHAR(45) NOT NULL,
+  team VARCHAR(200) NOT NULL,
+  goals INT NOT NULL,
+  CONSTRAINT fk_player
+        FOREIGN KEY(player_id) 
+                REFERENCES players(player_id),
+  CONSTRAINT fk_team
+        FOREIGN KEY(team_id) 
+	        REFERENCES all_time_team(team_id),
+  CONSTRAINT fk_season
+        FOREIGN KEY(season) 
+	        REFERENCES goals_per_season(season)
+
+);
+
+
+CREATE TABLE IF NOT EXISTS record_wins (
+  season VARCHAR(45),
+  seasons VARCHAR(45) NOT NULL,
+  Round VARCHAR(45) NOT NULL ,
+  date VARCHAR(45) NOT NULL,
+  Home VARCHAR(45) NOT NULL,
+  Result VARCHAR(45) NOT NULL,
+  Guest VARCHAR(45) NOT NULL,
+  CONSTRAINT fk_season
+        FOREIGN KEY(season) 
+	        REFERENCES goals_per_season(season)
+);
+
+
+CREATE TABLE IF NOT EXISTS top_scorers_all_time (
+  player_id UUID ,
+  player VARCHAR(45) NOT NULL,
+  teams VARCHAR(200) NOT NULL,
+  Matches INT NOT NULL,
+  goals INT NOT NULL,
+  Penalty INT NOT NULL,
+  Average FLOAT NOT NULL,
+  CONSTRAINT fk_player
+        FOREIGN KEY(player_id) 
+                REFERENCES players(player_id)
+
+
+);
+
+
+
 """
 cursor.execute(create_all_database_table)
 
@@ -214,6 +270,15 @@ COPY all_time_scorers_penalty (Player,Teams,Penalty)
 with open('./csv_dir/alltime_scorers(Penalty).csv', 'r') as f:
         cursor.copy_expert(sql=copy_all_time_scorers_penalty_table_sql, file=f)
 
+copy_all_time_scorers_own_goal_table_sql = """
+COPY all_time_scorers_own_goal (Player,Teams,own_goal)
+    FROM stdin
+    DELIMITER ','
+    CSV HEADER;
+    """
+with open('./csv_dir/alltime_scorers(own_goal).csv', 'r') as f:
+        cursor.copy_expert(sql=copy_all_time_scorers_own_goal_table_sql, file=f)
+
 copy_goals_per_season_table_sql = """
 COPY goals_per_season (Season,Goals,Matches,Average_Goal)
     FROM stdin
@@ -224,5 +289,50 @@ with open('./csv_dir/goals_per_season.csv', 'r') as f:
         cursor.copy_expert(sql=copy_goals_per_season_table_sql, file=f)
 
 
+copy_top_scorers_seasons_table_sql = """
+COPY top_scorers_seasons (Seasons,player,Team,goals)
+    FROM stdin
+    DELIMITER ','
+    CSV HEADER;
+    """
+with open('./csv_dir/top_scorers(seasons).csv', 'r') as f:
+        cursor.copy_expert(sql=copy_top_scorers_seasons_table_sql, file=f)
+
+
+copy_record_wins_table_sql = """
+COPY record_wins (Seasons,Round,date,Home,Result,Guest)
+    FROM stdin
+    DELIMITER ','
+    CSV HEADER;
+    """
+with open('./csv_dir/record_wins.csv', 'r') as f:
+        cursor.copy_expert(sql=copy_record_wins_table_sql, file=f)
+
+
+copy_top_scorers_all_time_table_sql = """
+COPY top_scorers_all_time (Player,Teams,Matches,goals,Penalty,Average)
+    FROM stdin
+    DELIMITER ','
+    CSV HEADER;
+    """
+with open('./csv_dir/top_scorers(all_time).csv', 'r') as f:
+        cursor.copy_expert(sql=copy_top_scorers_all_time_table_sql, file=f)
 
 conn.close()      
+
+conn_string = os.getenv('conn_string')
+db = create_engine(conn_string)
+conn = db.connect()
+
+detailed_top_scorers  = pd.read_csv("csv_dir/detail_top_scorers.csv")
+detailed_top_scorers.to_sql('detailed_top_scorers ', con=conn, if_exists='replace', index=False)
+
+top_scorers  = pd.read_csv("csv_dir/top_scorers.csv")
+top_scorers.to_sql('top_scorers', con=conn, if_exists='replace', index=False)
+
+assists  = pd.read_csv("csv_dir/assists.csv")
+assists.to_sql('assists', con=conn, if_exists='replace', index=False)
+
+league_table  = pd.read_csv("csv_dir/league_table.csv")
+league_table.to_sql('league_table', con=conn, if_exists='replace', index=False)
+conn.close()
